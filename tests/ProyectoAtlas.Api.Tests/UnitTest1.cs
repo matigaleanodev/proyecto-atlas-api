0f8900fded85b9bad94850b96b610c3a61049fff
Matias Galeano <99203645+matigaleanodev@users.noreply.github.com>
2026-03-19 20:51:51 -0300

    feat(tests): agregar estructura de pruebas unitarias y configuración de proyecto

---
+++ tests/ProyectoAtlas.Api.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿namespace ProyectoAtlas.Api.Tests;
+
+public class SmokeTests
+{
+    [Fact]
+    public void Should_pass_basic_assertion()
+    {
+        Assert.True(true);
+
+        Assert.False(false);
+    }
+}
